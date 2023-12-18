7688e43a0a0ce43926e71ce77a4dfe070cfa6792
Aleksandr Koshelev <ko4evneg@gmail.com>
2023-12-19 02:42:59 +0300

    Update enum

--- src/main/java/ru/trainithard/dunebot/model/ModType.java
+++ src/main/java/ru/trainithard/dunebot/model/ModType.java
@@ -1,5 +1,14 @@
 package ru.trainithard.dunebot.model;
 
+import lombok.Getter;
+
+@Getter
 public enum ModType {
-    CLASSIC, UPRISING
+    CLASSIC("\"Дюна (классика)\""), UPRISING_4("\"Апрайзинг на 4-х\""), UPRISING_6("\"Апрайзинг 3х3\"");
+
+    private final String modName;
+
+    ModType(String modName) {
+        this.modName = modName;
+    }
 }
