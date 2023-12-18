d9fa948d9584149bda15c7c92bb39070f72136bd
Aleksandr Koshelev <ko4evneg@gmail.com>
2023-12-18 14:22:13 +0300

    Initial commit

---
+++ src/main/java/ru/trainithard/dunebot/DunebotApplication.java
@@ -0,0 +1,13 @@
+package ru.trainithard.dunebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunebotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DunebotApplication.class, args);
+    }
+
+}
