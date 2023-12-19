3b78b8c8a03e8cc3a68340eb1d9c5448717e1d2a
Aleksandr Koshelev <ko4evneg@gmail.com>
2023-12-19 16:49:29 +0300

    Add jpa auditing

--- src/main/java/ru/trainithard/dunebot/DunebotApplication.java
+++ src/main/java/ru/trainithard/dunebot/DunebotApplication.java
@@ -1,29 +1,13 @@
 package ru.trainithard.dunebot;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-import ru.trainithard.dunebot.telegram.TelegramBot;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//todo
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class DunebotApplication implements CommandLineRunner {
-    @Value("${bot.token}")
-    private String botToken;
-    @Value("${bot.username}")
-    private String botUsername;
-
+@SpringBootApplication
+@EnableJpaAuditing
+public class DunebotApplication {
     public static void main(String[] args) {
         SpringApplication.run(DunebotApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(new TelegramBot(botUsername, botToken));
     }
 }
--- src/main/java/ru/trainithard/dunebot/model/BaseEntity.java
+++ src/main/java/ru/trainithard/dunebot/model/BaseEntity.java
@@ -1,18 +1,17 @@
 package ru.trainithard.dunebot.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public class BaseEntity {
     @Id
