3697c0e04c8328aa4b79b8a83bfffe820f1fa1cb
Aleksandr Koshelev <ko4evneg@gmail.com>
2023-12-19 16:38:20 +0300

    Add telegram constants

---
+++ src/main/java/ru/trainithard/dunebot/configuration/SettingConstants.java
@@ -0,0 +1,12 @@
+package ru.trainithard.dunebot.configuration;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SettingConstants {
+    // TODO:  replace to prod
+    public static final String CHAT_ID = "-1002139068680";
+    public static final int TOPIC_ID_CLASSIC = 2;
+    public static final int TOPIC_ID_UPRISING = 4;
+}
