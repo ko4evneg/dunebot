ccc4e8ef1a86a64bde3d4f8e07ab50c611dd769d
Aleksandr Koshelev <ko4evneg@gmail.com>
2023-12-20 02:18:55 +0300

    Add player naming test

---
+++ src/test/java/ru/trainithard/dunebot/model/PlayerTest.java
@@ -0,0 +1,34 @@
+package ru.trainithard.dunebot.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlayerTest {
+    private final Player player = new Player();
+
+    {
+        player.setSteamName("st_name");
+        player.setFirstName("f_name");
+    }
+
+    @Test
+    void shouldReturnMinimalRequiredName() {
+        assertEquals("st_name (f_name)", player.getFriendlyName());
+    }
+
+    @Test
+    void shouldReturnNameWithLastName() {
+        player.setLastName("l_name");
+
+        assertEquals("st_name (f_name l_name)", player.getFriendlyName());
+    }
+
+    @Test
+    void shouldReturnTelegramUsernameInstedOfPlainNames() {
+        player.setLastName("l_name");
+        player.setUserName("u_name");
+
+        assertEquals("st_name (u_name)", player.getFriendlyName());
+    }
+}
