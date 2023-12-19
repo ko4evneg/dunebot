8bf3b38303f6fe51bd756344e2fa78fb76838a83
Aleksandr Koshelev <ko4evneg@gmail.com>
2023-12-20 02:13:34 +0300

    Add telegramService cancel request

--- src/main/java/ru/trainithard/dunebot/service/telegram/TelegramService.java
+++ src/main/java/ru/trainithard/dunebot/service/telegram/TelegramService.java
@@ -3,6 +3,7 @@ package ru.trainithard.dunebot.service.telegram;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.polls.SendPoll;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -16,7 +17,12 @@ public class TelegramService {
     private final TelegramBot telegramBot;
 
     public void sendPoll(SendPoll sendPoll, BiConsumer<Message, Throwable> onCompleteAction) throws TelegramApiException {
-        CompletableFuture<Message> messageCompletableFuture = telegramBot.executeAsync(sendPoll);
-        messageCompletableFuture.whenComplete(onCompleteAction);
+        CompletableFuture<Message> sendMessageCompletableFuture = telegramBot.executeAsync(sendPoll);
+        sendMessageCompletableFuture.whenComplete(onCompleteAction);
+    }
+
+    public void deleteMessage(DeleteMessage deleteMessage, BiConsumer<Boolean, Throwable> onCompleteAction) throws TelegramApiException {
+        CompletableFuture<Boolean> deleteMessageCompletableFuture = telegramBot.executeAsync(deleteMessage);
+        deleteMessageCompletableFuture.whenComplete(onCompleteAction);
     }
 }
