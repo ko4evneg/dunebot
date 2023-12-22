c92cac214e6517f58319a2432633cf1cfd4c9072
Aleksandr Koshelev <ko4evneg@gmail.com>
2023-12-22 21:43:31 +0300

    Add exception

---
+++ src/main/java/ru/trainithard/dunebot/exception/DubeBotExsception.java
@@ -0,0 +1,7 @@
+package ru.trainithard.dunebot.exception;
+
+public class DubeBotExsception extends RuntimeException {
+    public DubeBotExsception(Throwable cause) {
+        super(cause);
+    }
+}
