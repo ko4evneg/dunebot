02a3ee3ef75ff0c4b02368f6ae43405220fd055d
Aleksandr Koshelev <ko4evneg@gmail.com>
2024-01-20 19:28:03 +0300

    Refactor command processors: SUBMIT RESUBMIT
    Fix bug for repeating match finish

--- src/main/java/ru/trainithard/dunebot/service/telegram/TelegramUpdateProcessor.java
+++ src/main/java/ru/trainithard/dunebot/service/telegram/TelegramUpdateProcessor.java
@@ -3,47 +3,50 @@ package ru.trainithard.dunebot.service.telegram;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.trainithard.dunebot.exception.AnswerableDuneBotException;
-import ru.trainithard.dunebot.model.Command;
 import ru.trainithard.dunebot.service.messaging.MessagingService;
 import ru.trainithard.dunebot.service.messaging.dto.MessageDto;
 import ru.trainithard.dunebot.service.telegram.command.CommandMessage;
 import ru.trainithard.dunebot.service.telegram.command.processor.CommandProcessor;
-
-import java.util.Map;
+import ru.trainithard.dunebot.service.telegram.factory.CommandMessageFactory;
+import ru.trainithard.dunebot.service.telegram.factory.CommandProcessorFactory;
+import ru.trainithard.dunebot.service.telegram.factory.ValidationStrategyFactory;
+import ru.trainithard.dunebot.service.telegram.validator.ValidationStrategy;
 
 @Service
 @RequiredArgsConstructor
 public class TelegramUpdateProcessor {
     private final TelegramBot telegramBot;
     private final MessagingService messagingService;
-    private final TelegramTextCommandValidator telegramTextCommandValidator;
-    private final Map<Command, CommandProcessor> commandProcessors;
+    private final CommandMessageFactory commandMessageFactory;
+    private final ValidationStrategyFactory validationStrategyFactory;
+    private final CommandProcessorFactory commandProcessorFactory;
 
     public void process() {
         Update update = telegramBot.poll();
         while (update != null) {
             try {
-                Message message = update.getMessage();
-                if (message != null && message.getText() != null && message.getText().startsWith("/")) {
-                    CommandMessage commandMessage = new CommandMessage(message);
-                    telegramTextCommandValidator.validate(commandMessage);
-                    commandProcessors.get(commandMessage.getCommand()).process(commandMessage);
-                } else if (update.hasPollAnswer()) {
-                    CommandMessage commandMessage = new CommandMessage(update.getPollAnswer());
-                    commandProcessors.get(commandMessage.getCommand()).process(commandMessage);
-                } else if (update.hasCallbackQuery()) {
-                    CommandMessage commandMessage = new CommandMessage(update.getCallbackQuery());
-                    commandProcessors.get(commandMessage.getCommand()).process(commandMessage);
+                CommandMessage commandMessage = commandMessageFactory.getInstance(update);
+                if (commandMessage != null) {
+                    validateCommandMessage(commandMessage);
+                    ValidationStrategy validator = validationStrategyFactory.getValidator(commandMessage.getCommand().getCommandType());
+                    validator.validate(commandMessage);
+                    CommandProcessor processor = commandProcessorFactory.getProcessor(commandMessage.getCommand());
+                    processor.process(commandMessage);
                 }
             } catch (AnswerableDuneBotException answerableException) {
                 sendUserNotificationMessage(answerableException);
-            } catch (Exception e) {
+            } catch (Exception ignored) {
             } finally {
                 update = telegramBot.poll();
             }
+        }
+    }
+
+    private void validateCommandMessage(CommandMessage commandMessage) {
+        if (commandMessage.getCommand() == null) {
+            throw new AnswerableDuneBotException("Неверная команда!", commandMessage);
         }
     }
 
@@ -54,7 +57,7 @@ public class TelegramUpdateProcessor {
             sendMessage.setText(answerableException.getMessage());
             MessageDto messageDto = new MessageDto(Long.toString(answerableException.getTelegramChatId()), answerableException.getMessage(), answerableException.getTelegramReplyId(), null);
             messagingService.sendMessageAsync(messageDto);
-        } catch (Exception e) {
+        } catch (Exception ignored) {
         }
     }
 }
--- src/test/java/ru/trainithard/dunebot/service/telegram/TelegramUpdateProcessorTest.java
+++ src/test/java/ru/trainithard/dunebot/service/telegram/TelegramUpdateProcessorTest.java
@@ -16,39 +16,50 @@ import org.telegram.telegrambots.meta.api.objects.polls.PollAnswer;
 import ru.trainithard.dunebot.TestContextMock;
 import ru.trainithard.dunebot.exception.DuneBotException;
 import ru.trainithard.dunebot.model.Command;
-import ru.trainithard.dunebot.model.ModType;
+import ru.trainithard.dunebot.model.CommandType;
 import ru.trainithard.dunebot.model.messaging.ChatType;
 import ru.trainithard.dunebot.service.messaging.MessagingService;
 import ru.trainithard.dunebot.service.messaging.dto.ExternalPollDto;
 import ru.trainithard.dunebot.service.messaging.dto.PollMessageDto;
-import ru.trainithard.dunebot.service.telegram.command.CommandMessage;
+import ru.trainithard.dunebot.service.telegram.factory.CommandMessageFactory;
+import ru.trainithard.dunebot.service.telegram.factory.CommandProcessorFactory;
+import ru.trainithard.dunebot.service.telegram.factory.ValidationStrategyFactory;
+import ru.trainithard.dunebot.service.telegram.validator.DefaultCommandMessageValidator;
 
 import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class TelegramUpdateProcessorTest extends TestContextMock {
-    @Autowired
-    private TelegramUpdateProcessor updateProcessor;
-    @MockBean
-    private MessagingService messagingService;
-    @MockBean
-    private TelegramTextCommandValidator validator;
-
-    private static final int TELEGRAM_REPLY_ID = 10010;
     private static final String COMMAND_REFRESH_PROFILE = "/refresh_profile";
+    private static final int TELEGRAM_REPLY_ID = 10010;
     private static final long TELEGRAM_USER_ID_1 = 10000L;
     private static final long TELEGRAM_USER_ID_2 = 10001L;
     private static final long TELEGRAM_CHAT_ID_1 = 9000L;
     private static final long TELEGRAM_CHAT_ID_2 = 9001L;
 
+    @Autowired
+    private TelegramUpdateProcessor updateProcessor;
+    @Autowired
+    private DefaultCommandMessageValidator defaultValidator;
+    @MockBean
+    private MessagingService messagingService;
+    @MockBean
+    private ValidationStrategyFactory validationStrategyFactory;
+    @MockBean
+    private CommandProcessorFactory commandProcessorFactory;
+    @MockBean
+    private CommandMessageFactory commandMessageFactory;
+
     @BeforeEach
     void beforeEach() {
+        doCallRealMethod().when(commandMessageFactory).getInstance(any());
+
         jdbcTemplate.execute("insert into players (id, external_id, external_chat_id, steam_name, first_name, created_at) " +
                 "values (10000, " + TELEGRAM_USER_ID_1 + ", " + TELEGRAM_CHAT_ID_1 + " , 'st_pl1', 'name1', '2010-10-10') ");
         jdbcTemplate.execute("insert into players (id, external_id, external_chat_id, steam_name, first_name, created_at) " +
@@ -64,141 +75,42 @@ class TelegramUpdateProcessorTest extends TestContextMock {
     }
 
     @Test
-    void shouldInvokeProcessorOnValidTextCommand() {
-        when(telegramBot.poll()).thenReturn(getUpdate(TELEGRAM_USER_ID_1, TELEGRAM_CHAT_ID_1, null, COMMAND_REFRESH_PROFILE)).thenReturn(null);
-        doReturn(getCompletableFuturePollMessage()).when(messagingService).sendPollAsync(ArgumentMatchers.any(PollMessageDto.class));
-        doNothing().when(validator).validate(any());
-
-        updateProcessor.process();
-
-        String updatedName = jdbcTemplate.queryForObject("select first_name from players where id = 10000", String.class);
-
-        assertEquals("newFirstName", updatedName);
-    }
-
-    @Test
-    void shouldInvokeProcessorOnValidVoteCommand() {
-        when(telegramBot.poll()).thenReturn(getPollAnswerUpdate()).thenReturn(null);
-        doReturn(getCompletableFuturePollMessage()).when(messagingService).sendPollAsync(ArgumentMatchers.any(PollMessageDto.class));
-        doNothing().when(validator).validate(any());
-        jdbcTemplate.execute("insert into external_messages (id, dtype, message_id, chat_id, poll_id, created_at) " +
-                "values (10000, 'ExternalPollId', 10000, 10000, '100001', '2020-10-10')");
-        jdbcTemplate.execute("insert into matches (id, external_poll_id, owner_id, mod_type, positive_answers_count, created_at) " +
-                "values (10000, 10000, 10000, '" + ModType.CLASSIC + "', 0, '2010-10-10') ");
-
-        updateProcessor.process();
-
-        Integer actualPositiveAnswers = jdbcTemplate.queryForObject("select positive_answers_count from matches where id = 10000", Integer.class);
-        Long actualMatchPlayerId = jdbcTemplate.queryForObject("select player_id from match_players where match_id = 10000", Long.class);
-
-        assertEquals(1, actualPositiveAnswers);
-        assertEquals(TELEGRAM_USER_ID_1, actualMatchPlayerId);
-    }
-
-    private Update getPollAnswerUpdate() {
-        User user = new User();
-        user.setId(TELEGRAM_USER_ID_1);
-        PollAnswer pollAnswer = new PollAnswer();
-        pollAnswer.setUser(user);
-        pollAnswer.setOptionIds(Collections.singletonList(0));
-        pollAnswer.setPollId("100001");
-        Update update = new Update();
-        update.setPollAnswer(pollAnswer);
-        return update;
-    }
-
-    private CompletableFuture<ExternalPollDto> getCompletableFuturePollMessage() {
-        Poll poll = new Poll();
-        poll.setId("100001");
-        Message message = new Message();
-        message.setPoll(poll);
-        message.setMessageId(10000);
-        Chat chat = new Chat();
-        chat.setId(10000L);
-        message.setChat(chat);
-        CompletableFuture<Message> completableFuture = new CompletableFuture<>();
-        completableFuture.complete(message);
-        return CompletableFuture.completedFuture(new ExternalPollDto(message));
-    }
-
-    @Test
-    void shouldInvokeProcessorOnValidCallbackQueryCommand() {
-        when(telegramBot.poll()).thenReturn(getCallbackQueryUpdate()).thenReturn(null);
-        jdbcTemplate.execute("insert into external_messages (id, dtype, message_id, chat_id, poll_id, created_at) " +
-                "values (10000, 'ExternalPollId', 10000, 10000, '100001', '2020-10-10')");
-        jdbcTemplate.execute("insert into matches (id, owner_id, mod_type, positive_answers_count, created_at) " +
-                "values (10000, 10000, '" + ModType.CLASSIC + "', 0, '2010-10-10') ");
-        jdbcTemplate.execute("insert into match_players (id, match_id, player_id, external_submit_id, created_at) " +
-                "values (10000, 10000, 10000, 10000, '2020-10-10')");
-
-        updateProcessor.process();
-
-        Integer actualCandidatePlace = jdbcTemplate.queryForObject("select candidate_place from match_players where player_id = " +
-                "(select id from players where external_id = " + TELEGRAM_USER_ID_1 + ")", Integer.class);
-
-        assertEquals(-1, actualCandidatePlace);
-    }
-
-    private Update getCallbackQueryUpdate() {
-        User user = new User();
-        user.setId(TELEGRAM_USER_ID_1);
-        Message message = new Message();
-        message.setMessageId(TELEGRAM_REPLY_ID);
-        message.setFrom(user);
-        CallbackQuery callbackQuery = new CallbackQuery();
-        callbackQuery.setMessage(message);
-        callbackQuery.setData("10000__-1");
-        callbackQuery.setFrom(user);
-        Update update = new Update();
-        update.setCallbackQuery(callbackQuery);
-        return update;
-    }
-
-    @Test
-    void shouldSendTelegramMessageOnWrongCommand() {
-        when(telegramBot.poll()).thenReturn(getUpdate(TELEGRAM_USER_ID_1, TELEGRAM_CHAT_ID_1, null, "/wrongcommand")).thenReturn(null);
-        doCallRealMethod().when(validator).validate(any());
+    void shouldSendTelegramMessageOnUnknownCommandException() {
+        when(telegramBot.poll()).thenReturn(getTextUpdate(TELEGRAM_USER_ID_1, TELEGRAM_CHAT_ID_1, null, "/wrongcommand")).thenReturn(null);
 
         updateProcessor.process();
 
         verify(messagingService, times(1)).sendMessageAsync(argThat(messageDto ->
-                "9000".equals(messageDto.getChatId()) && "Неверная команда!".equals(messageDto.getText())));
+                Long.toString(TELEGRAM_CHAT_ID_1).equals(messageDto.getChatId()) && "Неверная команда!".equals(messageDto.getText())));
     }
 
     @Test
     void shouldIncludeReplyMessageIdOnTopicWrongCommandReceive() {
-        when(telegramBot.poll()).thenReturn(getUpdate(TELEGRAM_USER_ID_1, TELEGRAM_CHAT_ID_1, 12345, "/wrongcommand")).thenReturn(null);
-        doCallRealMethod().when(validator).validate(any());
+        when(telegramBot.poll()).thenReturn(getTextUpdate(TELEGRAM_USER_ID_1, TELEGRAM_CHAT_ID_1, 12345, "/wrongcommand")).thenReturn(null);
 
         updateProcessor.process();
 
         verify(messagingService, times(1)).sendMessageAsync(argThat(messageDto ->
-                messageDto.getReplyMessageId().equals(12345)));
+                messageDto.getReplyMessageId() == 12345));
     }
 
-    @ParameterizedTest
-    @MethodSource("exceptionsProvider")
-    void shouldNotThrowOnException(Class<? extends Exception> aClass) {
-        when(telegramBot.poll()).thenReturn(getUpdate(TELEGRAM_USER_ID_1, TELEGRAM_CHAT_ID_1, null, "/up")).thenReturn(null);
-        doThrow(aClass).when(validator).validate(any(CommandMessage.class));
-
-        assertDoesNotThrow(() -> updateProcessor.process());
-    }
-
-    @ParameterizedTest
-    @MethodSource("exceptionsProvider")
-    void shouldMoveToNextUpdateOnException(Class<? extends Exception> aClass) {
-        when(telegramBot.poll())
-                .thenReturn(getUpdate(TELEGRAM_USER_ID_1, TELEGRAM_CHAT_ID_1, null, "/up"))
-                .thenReturn(getUpdate(TELEGRAM_USER_ID_2, TELEGRAM_CHAT_ID_2, null, COMMAND_REFRESH_PROFILE)).thenReturn(null);
-        doThrow(aClass).when(validator).validate(any(CommandMessage.class));
+    @Test
+    void shouldDoNothingOnNotAcceptableUpdate() {
+        when(telegramBot.poll()).thenReturn(getTextUpdate(TELEGRAM_USER_ID_1, TELEGRAM_CHAT_ID_1, null, "just text")).thenReturn(null);
 
         updateProcessor.process();
 
-        verify(validator, times(1)).validate(argThat(commandMessage ->
-                TELEGRAM_USER_ID_2 == commandMessage.getUserId() &&
-                        TELEGRAM_CHAT_ID_2 == commandMessage.getChatId() &&
-                        commandMessage.getCommand() == Command.REFRESH_PROFILE));
+        verify(validationStrategyFactory, never()).getValidator(any());
+        verify(commandProcessorFactory, never()).getProcessor(any());
+    }
+
+    @ParameterizedTest
+    @MethodSource("exceptionsProvider")
+    void shouldNotThrowOnException(Class<? extends Exception> anException) {
+        when(telegramBot.poll()).thenReturn(getTextUpdate(TELEGRAM_USER_ID_1, TELEGRAM_CHAT_ID_1, null, "/up")).thenReturn(null);
+        doThrow(anException).when(commandMessageFactory).getInstance(any());
+
+        assertDoesNotThrow(() -> updateProcessor.process());
     }
 
     private static Stream<Arguments> exceptionsProvider() {
@@ -210,20 +122,70 @@ class TelegramUpdateProcessorTest extends TestContextMock {
     }
 
     @Test
-    void shouldMoveToNextUpdateWithoutException() {
+    void shouldMoveToNextUpdateWhenExceptionOccurs() {
         when(telegramBot.poll())
-                .thenReturn(getUpdate(TELEGRAM_USER_ID_1, TELEGRAM_CHAT_ID_1, null, COMMAND_REFRESH_PROFILE))
-                .thenReturn(getUpdate(TELEGRAM_USER_ID_2, TELEGRAM_CHAT_ID_2, null, COMMAND_REFRESH_PROFILE)).thenReturn(null);
+                .thenReturn(getTextUpdate(TELEGRAM_USER_ID_1, TELEGRAM_CHAT_ID_1, null, COMMAND_REFRESH_PROFILE))
+                .thenReturn(getTextUpdate(TELEGRAM_USER_ID_2, TELEGRAM_CHAT_ID_2, null, COMMAND_REFRESH_PROFILE)).thenReturn(null);
+        doThrow(new RuntimeException()).when(commandMessageFactory).getInstance(any());
 
         updateProcessor.process();
 
-        verify(validator, times(1)).validate(argThat(commandMessage ->
-                TELEGRAM_USER_ID_1 == commandMessage.getUserId() && TELEGRAM_CHAT_ID_1 == commandMessage.getChatId()));
-        verify(validator, times(1)).validate(argThat(commandMessage ->
-                TELEGRAM_USER_ID_2 == commandMessage.getUserId() && TELEGRAM_CHAT_ID_2 == commandMessage.getChatId()));
+        verify(commandMessageFactory, times(1)).getInstance(argThat(update ->
+                TELEGRAM_USER_ID_1 == update.getMessage().getFrom().getId() && TELEGRAM_CHAT_ID_1 == update.getMessage().getChatId()));
+        verify(commandMessageFactory, times(1)).getInstance(argThat(update ->
+                TELEGRAM_USER_ID_2 == update.getMessage().getFrom().getId() && TELEGRAM_CHAT_ID_2 == update.getMessage().getChatId()));
     }
 
-    private Update getUpdate(long telegramUserId, long telegramChatId, Integer replyId, String text) {
+    @Test
+    void shouldDoSomethingOnException() {
+        fail();
+    }
+
+    @Test
+    void shouldDoSomethingOnMessagingServiceException() {
+        fail();
+    }
+
+    @ParameterizedTest
+    @MethodSource({"validatorsSource"})
+    void shouldCallValidationStrategyFactoryOnUpdate(Update update, CommandType expectedCommandType) {
+        when(telegramBot.poll()).thenReturn(update).thenReturn(null);
+        doReturn(getCompletableFuturePollMessage()).when(messagingService).sendPollAsync(ArgumentMatchers.any(PollMessageDto.class));
+
+        updateProcessor.process();
+
+        verify(validationStrategyFactory, times(1)).getValidator(eq(expectedCommandType));
+    }
+
+    private static Stream<Arguments> validatorsSource() {
+        return Stream.of(
+                Arguments.of(getTextUpdate(TELEGRAM_USER_ID_1, TELEGRAM_CHAT_ID_1, null, COMMAND_REFRESH_PROFILE), CommandType.TEXT),
+                Arguments.of(getPollAnswerUpdate(), CommandType.POLL_VOTE),
+                Arguments.of(getCallbackQueryUpdate(), CommandType.CALLBACK)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource({"processorsSource"})
+    void shouldCallProcessorForUpdate(Update update, Command expectedCommand) {
+        when(telegramBot.poll()).thenReturn(update).thenReturn(null);
+        doReturn(defaultValidator).when(validationStrategyFactory).getValidator(any());
+        doReturn(getCompletableFuturePollMessage()).when(messagingService).sendPollAsync(ArgumentMatchers.any(PollMessageDto.class));
+
+        updateProcessor.process();
+
+        verify(commandProcessorFactory, times(1)).getProcessor(eq(expectedCommand));
+    }
+
+    private static Stream<Arguments> processorsSource() {
+        return Stream.of(
+                Arguments.of(getTextUpdate(TELEGRAM_USER_ID_1, TELEGRAM_CHAT_ID_1, null, COMMAND_REFRESH_PROFILE), Command.REFRESH_PROFILE),
+                Arguments.of(getPollAnswerUpdate(), Command.VOTE),
+                Arguments.of(getCallbackQueryUpdate(), Command.ACCEPT_SUBMIT)
+        );
+    }
+
+    private static Update getTextUpdate(long telegramUserId, long telegramChatId, Integer replyId, String text) {
         User user = new User();
         user.setId(telegramUserId);
         user.setFirstName("newFirstName");
@@ -241,5 +203,46 @@ class TelegramUpdateProcessorTest extends TestContextMock {
         Update update = new Update();
         update.setMessage(message);
         return update;
+    }
+
+    private static Update getPollAnswerUpdate() {
+        User user = new User();
+        user.setId(TELEGRAM_USER_ID_1);
+        PollAnswer pollAnswer = new PollAnswer();
+        pollAnswer.setUser(user);
+        pollAnswer.setOptionIds(Collections.singletonList(0));
+        pollAnswer.setPollId("100001");
+        Update update = new Update();
+        update.setPollAnswer(pollAnswer);
+        return update;
+    }
+
+    private static Update getCallbackQueryUpdate() {
+        User user = new User();
+        user.setId(TELEGRAM_USER_ID_1);
+        Message message = new Message();
+        message.setMessageId(TELEGRAM_REPLY_ID);
+        message.setFrom(user);
+        CallbackQuery callbackQuery = new CallbackQuery();
+        callbackQuery.setMessage(message);
+        callbackQuery.setData("10000__-1");
+        callbackQuery.setFrom(user);
+        Update update = new Update();
+        update.setCallbackQuery(callbackQuery);
+        return update;
+    }
+
+    private CompletableFuture<ExternalPollDto> getCompletableFuturePollMessage() {
+        Poll poll = new Poll();
+        poll.setId("100001");
+        Message message = new Message();
+        message.setPoll(poll);
+        message.setMessageId(10000);
+        Chat chat = new Chat();
+        chat.setId(10000L);
+        message.setChat(chat);
+        CompletableFuture<Message> completableFuture = new CompletableFuture<>();
+        completableFuture.complete(message);
+        return CompletableFuture.completedFuture(new ExternalPollDto(message));
     }
 }
