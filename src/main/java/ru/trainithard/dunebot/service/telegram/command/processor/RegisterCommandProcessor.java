97831a1a974c11df773ed6d5c428ffe63959d743
Aleksandr Koshelev <ko4evneg@gmail.com>
2024-01-27 18:39:29 +0300

    Update Registration processor for input names instead of using telegram names

--- src/main/java/ru/trainithard/dunebot/service/telegram/command/processor/RegisterCommandProcessor.java
+++ src/main/java/ru/trainithard/dunebot/service/telegram/command/processor/RegisterCommandProcessor.java
@@ -5,12 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.trainithard.dunebot.exception.AnswerableDuneBotException;
+import ru.trainithard.dunebot.exception.WrongNamesInputException;
 import ru.trainithard.dunebot.model.Player;
 import ru.trainithard.dunebot.repository.PlayerRepository;
 import ru.trainithard.dunebot.service.messaging.MessagingService;
 import ru.trainithard.dunebot.service.messaging.dto.MessageDto;
 import ru.trainithard.dunebot.service.telegram.command.Command;
 import ru.trainithard.dunebot.service.telegram.command.CommandMessage;
+import ru.trainithard.dunebot.util.ParsedNames;
 
 @Service
 @RequiredArgsConstructor
@@ -18,8 +20,8 @@ public class RegisterCommandProcessor extends CommandProcessor {
     private static final Logger logger = LoggerFactory.getLogger(RegisterCommandProcessor.class);
     private static final String NICKNAME_IS_BUSY_MESSAGE_TEMPLATE = "Пользователь со steam ником %s уже существует!";
     private static final String ALREADY_REGISTERED_MESSAGE_TEMPLATE = "Вы уже зарегистрированы под steam ником %s! " +
-            "Для смены ника выполните команду '/change_steam_name *new_name*'";
-    private static final String REGISTRATION_MESSAGE_TEMPLATE = "Вы зарегистрированы под steam-именем ";
+            "Для смены ника выполните команду '/refresh_profile *Имя* (*steam никнейм*) *Фамилия*'";
+    private static final String REGISTRATION_MESSAGE_TEMPLATE = "Вы зарегистрированы как ";
 
     private final PlayerRepository playerRepository;
     private final MessagingService messagingService;
@@ -28,27 +30,33 @@ public class RegisterCommandProcessor extends CommandProcessor {
     public void process(CommandMessage commandMessage, int loggingId) {
         logger.debug("{}: register started", loggingId);
 
-        validate(commandMessage);
+        ParsedNames parsedNames = validate(commandMessage);
         logger.debug("{}: validation passed", loggingId);
 
-        Player player = playerRepository.save(new Player(commandMessage));
-        String messageText = REGISTRATION_MESSAGE_TEMPLATE + player.getSteamName();
+        Player player = playerRepository.save(new Player(commandMessage, parsedNames));
+        String messageText = REGISTRATION_MESSAGE_TEMPLATE + player.getFriendlyName();
         messagingService.sendMessageAsync(
                 new MessageDto(commandMessage.getChatId(), messageText, commandMessage.getReplyMessageId(), null));
 
         logger.debug("{}: register ended", loggingId);
     }
 
-    private void validate(CommandMessage commandMessage) {
-        long externalId = commandMessage.getUserId();
-        String steamName = commandMessage.getAllArguments();
-        playerRepository.findByExternalIdOrSteamName(externalId, steamName).ifPresent(player -> {
-            if (externalId == player.getExternalId()) {
-                throw new AnswerableDuneBotException(String.format(ALREADY_REGISTERED_MESSAGE_TEMPLATE, player.getSteamName()), player.getExternalChatId());
-            } else if (steamName.equals(player.getSteamName())) {
-                throw new AnswerableDuneBotException(String.format(NICKNAME_IS_BUSY_MESSAGE_TEMPLATE, steamName), player.getExternalChatId());
-            }
-        });
+    private ParsedNames validate(CommandMessage commandMessage) {
+        try {
+            long externalId = commandMessage.getUserId();
+            ParsedNames parsedNames = new ParsedNames(commandMessage.getAllArguments());
+            String steamName = parsedNames.getSteamName();
+            playerRepository.findByExternalIdOrSteamName(externalId, steamName).ifPresent(player -> {
+                if (externalId == player.getExternalId()) {
+                    throw new AnswerableDuneBotException(String.format(ALREADY_REGISTERED_MESSAGE_TEMPLATE, player.getSteamName()), player.getExternalChatId());
+                } else if (steamName.equals(player.getSteamName())) {
+                    throw new AnswerableDuneBotException(String.format(NICKNAME_IS_BUSY_MESSAGE_TEMPLATE, steamName), player.getExternalChatId());
+                }
+            });
+            return parsedNames;
+        } catch (WrongNamesInputException exception) {
+            throw new AnswerableDuneBotException(exception.getMessage(), commandMessage);
+        }
     }
 
     @Override
