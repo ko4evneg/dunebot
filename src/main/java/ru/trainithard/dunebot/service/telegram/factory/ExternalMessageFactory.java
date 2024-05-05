4a149faba1cb94a8ec73c34a752bf103e710d7a5
Aleksandr Koshelev <ko4evneg@gmail.com>
2024-05-06 02:46:12 +0300

    Update MatchFinishingServiceImpl API and add message factory usage Add finish message to message factory

--- src/main/java/ru/trainithard/dunebot/service/telegram/factory/ExternalMessageFactory.java
+++ src/main/java/ru/trainithard/dunebot/service/telegram/factory/ExternalMessageFactory.java
@@ -1,8 +1,11 @@
 package ru.trainithard.dunebot.service.telegram.factory;
 
+import ru.trainithard.dunebot.model.Match;
 import ru.trainithard.dunebot.model.Player;
 import ru.trainithard.dunebot.service.messaging.ExternalMessage;
 
 public interface ExternalMessageFactory {
     ExternalMessage getGuestMessageDto(Player player);
+
+    ExternalMessage getFinishReasonMessage(Match match, boolean isFailedByResubmitsLimit);
 }
