f4b22d65883c5c1b2d7f58ac45b7cb9ddd91676c
Aleksandr Koshelev <ko4evneg@gmail.com>
2024-05-07 01:59:41 +0300

    Fix merge

--- src/main/java/ru/trainithard/dunebot/service/telegram/command/processor/HelpCommandProcessor.java
+++ src/main/java/ru/trainithard/dunebot/service/telegram/command/processor/HelpCommandProcessor.java
@@ -2,12 +2,12 @@ package ru.trainithard.dunebot.service.telegram.command.processor;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.trainithard.dunebot.service.messaging.ExternalMessage;
 import ru.trainithard.dunebot.service.messaging.dto.MessageDto;
 import ru.trainithard.dunebot.service.telegram.command.Command;
 import ru.trainithard.dunebot.service.telegram.command.CommandMessage;
+import ru.trainithard.dunebot.service.telegram.factory.messaging.ExternalMessageFactory;
 
 /**
  * Shows bot how to instructions to requester.
@@ -16,40 +16,18 @@ import ru.trainithard.dunebot.service.telegram.command.CommandMessage;
 @Service
 @RequiredArgsConstructor
 public class HelpCommandProcessor extends CommandProcessor {
-    @Value("${bot.version}")
-    private String version;
+    private final ExternalMessageFactory messageFactory;
 
     @Override
     public void process(CommandMessage commandMessage) {
         log.debug("{}: HELP started", logId());
-        messagingService.sendMessageAsync(new MessageDto(commandMessage, getHelpText(), null));
+        ExternalMessage helpMessage = messageFactory.getHelpMessage();
+        messagingService.sendMessageAsync(new MessageDto(commandMessage, helpMessage, null));
         log.debug("{}: HELP ended", logId());
     }
 
     @Override
     public Command getCommand() {
         return Command.HELP;
-    }
-
-    private ExternalMessage getHelpText() {
-        return new ExternalMessage()
-                .startBold().append("Dunebot v").append(version).endBold().newLine()
-                .appendLink("Подробное описание бота", "https://github.com/ko4evneg/dunebot/blob/master/help.md").newLine()
-                .appendLink("История изменений", "https://github.com/ko4evneg/dunebot/blob/master/changelog.txt")
-                .newLine().newLine()
-                .append("Список доступных команд:").newLine()
-                .append("'/profile Имя (ник_steam) Фамилия' Регистрация игрока в рейтинге").newLine()
-                .append("'/profile Имя (ник_steam) Фамилия' Изменение данных существующего игрока").newLine()
-                .append("'/profile' Обновление данных Telegram (только для зарегистрированных игроков)").newLine()
-                .append("'/new_dune' Создание опроса для классической Dune").newLine()
-                .append("'/new_up4' Создание опроса для Dune Uprising (4 игрока)").newLine()
-                .append("'/new_up6' Создание опроса для Dune Uprising (6 игроков). " +
-                        "Только для сбора матча по опросу. Результаты не собираются.").newLine()
-                .append("'/cancel' Удаление последнего опроса, созданного игроком").newLine()
-                .append("'/submit ").appendBold("ID_игры").append("' Запуск регистрации результатов игры с номером ")
-                .appendBold("ID_игры").newLine()
-                .append("'/resubmit ").appendBold("ID_игры")
-                .append("' Запуск регистрации результатов игры заново. Возможно выполнить до трех раз на игру").newLine()
-                .append("'/help' Помощь");
     }
 }
