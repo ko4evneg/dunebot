64828461930414768c529a314090be10da9ba325
Aleksandr Koshelev <ko4evneg@gmail.com>
2024-05-12 17:21:33 +0300

    Add caching

--- src/main/java/ru/trainithard/dunebot/DunebotApplication.java
+++ src/main/java/ru/trainithard/dunebot/DunebotApplication.java
@@ -2,12 +2,14 @@ package ru.trainithard.dunebot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 @EnableAsync
 @EnableScheduling
 public class DunebotApplication {
