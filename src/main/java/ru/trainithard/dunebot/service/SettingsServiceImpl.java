1d21925632746ac1f6c6d1c772da77da143e1c45
Aleksandr Koshelev <ko4evneg@gmail.com>
2024-05-12 17:36:17 +0300

    Feat/caching (#75)
    * Add caching
    * Add settings service caching

--- src/main/java/ru/trainithard/dunebot/service/SettingsServiceImpl.java
+++ src/main/java/ru/trainithard/dunebot/service/SettingsServiceImpl.java
@@ -1,7 +1,10 @@
 package ru.trainithard.dunebot.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.trainithard.dunebot.model.Setting;
 import ru.trainithard.dunebot.model.SettingKey;
 import ru.trainithard.dunebot.repository.SettingRepository;
@@ -12,24 +15,29 @@ public class SettingsServiceImpl implements SettingsService {
     private final SettingRepository settingRepository;
 
     @Override
+    @Cacheable(value = "settings", key = "#key")
     public Integer getIntSetting(SettingKey key) {
         Setting setting = settingRepository.findByKey(key);
         return setting == null ? null : Integer.parseInt(setting.getValue());
     }
 
     @Override
+    @Cacheable(value = "settings", key = "#key")
     public long getLongSetting(SettingKey key) {
         String value = settingRepository.findByKey(key).getValue();
         return Long.parseLong(value);
     }
 
     @Override
+    @Cacheable(value = "settings", key = "#key")
     public String getStringSetting(SettingKey key) {
         Setting setting = settingRepository.findByKey(key);
         return setting == null ? null : setting.getValue();
     }
 
     @Override
+    @CacheEvict(value = "settings", key = "#key")
+    @Transactional
     public void saveSetting(SettingKey key, String value) {
         Setting existingSetting = settingRepository.findByKey(key);
         if (existingSetting != null) {
--- src/test/java/ru/trainithard/dunebot/TestContextMock.java
+++ src/test/java/ru/trainithard/dunebot/TestContextMock.java
@@ -1,16 +1,27 @@
 package ru.trainithard.dunebot;
 
+import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cache.CacheManager;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ru.trainithard.dunebot.service.messaging.MessagingService;
 import ru.trainithard.dunebot.service.telegram.TelegramBot;
 
+import java.util.Objects;
+
 public abstract class TestContextMock {
     @Autowired
     protected JdbcTemplate jdbcTemplate;
+    @Autowired
+    protected CacheManager cacheManager;
     @MockBean
     protected TelegramBot telegramBot;
     @MockBean
     protected MessagingService messagingService;
+
+    @AfterEach
+    void superAfterEach() {
+        Objects.requireNonNull(cacheManager.getCache("settings")).invalidate();
+    }
 }
