a201df5efa620fa70a0d24d197b4f560170b5fe6
Aleksandr Koshelev <ko4evneg@gmail.com>
2024-05-13 16:04:01 +0300

    Add /config command

--- src/main/java/ru/trainithard/dunebot/model/UserSettingKey.java
+++ src/main/java/ru/trainithard/dunebot/model/UserSettingKey.java
@@ -3,6 +3,7 @@ package ru.trainithard.dunebot.model;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 /**
  * Setting key for user setting values
@@ -16,13 +17,13 @@ public enum UserSettingKey {
 
     private final String friendlyName;
 
-    public static UserSettingKey getByName(String settingName) {
+    public static Optional<UserSettingKey> getByName(String settingName) {
         if (settingName == null || settingName.isBlank()) {
             return null;
         }
 
         return Arrays.stream(values())
                 .filter(settingKey -> settingName.equalsIgnoreCase(settingKey.name()))
-                .findFirst().orElse(null);
+                .findFirst();
     }
 }
