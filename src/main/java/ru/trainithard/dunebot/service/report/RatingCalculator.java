4427ea80e86382852c2649077a1fbbc35d17c0a6
Aleksandr Koshelev <ko4evneg@gmail.com>
2024-07-29 02:27:17 +0400

    Add RatingCaluclator based on rating method

--- src/main/java/ru/trainithard/dunebot/service/report/RatingCalculator.java
+++ src/main/java/ru/trainithard/dunebot/service/report/RatingCalculator.java
@@ -1,9 +1,11 @@
 package ru.trainithard.dunebot.service.report;
 
+import ru.trainithard.dunebot.model.AbstractRating;
+
 import java.util.HashMap;
 import java.util.Map;
 
-class RatingCalculator {
+public class RatingCalculator {
     private static final Map<Integer, Double> efficiencyRateByPlaceNames = new HashMap<>();
 
     static {
@@ -14,6 +16,26 @@ class RatingCalculator {
     }
 
     private RatingCalculator() {
+    }
+
+    public static double calculateEfficiency(AbstractRating rating) {
+        Map<Integer, Integer> placeCountByPlaceNames = new HashMap<>();
+        placeCountByPlaceNames.put(1, rating.getFirstPlaceCount());
+        placeCountByPlaceNames.put(2, rating.getSecondPlaceCount());
+        placeCountByPlaceNames.put(3, rating.getThirdPlaceCount());
+        placeCountByPlaceNames.put(4, rating.getFourthPlaceCount());
+        return calculateEfficiencyNew(placeCountByPlaceNames, rating.getMatchesCount());
+    }
+
+    private static double calculateEfficiencyNew(Map<Integer, Integer> placeCountByPlaceNames, long allPlacesCount) {
+        double efficiencesSum = 0;
+        for (Map.Entry<Integer, Integer> entry : placeCountByPlaceNames.entrySet()) {
+            int place = entry.getKey();
+            double placeEfficiency = efficiencyRateByPlaceNames.getOrDefault(place, 1.0);
+            double placesCount = placeCountByPlaceNames.getOrDefault(place, 0);
+            efficiencesSum += placesCount / allPlacesCount * placeEfficiency;
+        }
+        return ((int) (efficiencesSum * 100)) / 100.0;
     }
 
     static double calculateEfficiency(Map<Integer, Long> placeCountByPlaceNames, long allPlacesCount) {
